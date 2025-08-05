7baa10c4de8b605e4ddceb2fbdd0e580377b5f01
Fernando Dias <fernando.sdbarros@gmail.com>
2025-08-05 08:24:35 -0300

    add crud operation delete

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -81,8 +81,21 @@ exports.updateTour = async (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'sucess',
-        data: null
-    })
+
 };
+
+exports.deleteTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'sucess',
+            data: null
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error.message
+        });
+    }
+}

cc27e606162d2d2c2393f380ddefac3fe0fd6208
Fernando Dias <fernando.sdbarros@gmail.com>
2025-09-05 08:45:37 -0300

    learning about error handling

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const appError = require('./utils/appError');
+const globalErrorHandler = require ('./controllers/errorController.js');
 const toursRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,19 +18,20 @@ app.use(morgan('dev')); // Use morgan for logging in development mode
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.static(`${__dirname}/public`)); // Serve static files from the public directory
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next(); // Call next() to pass control to the next middleware
-});
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next(); // Call next() to pass control to the next middleware
 });
 
-
 // routes
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new appError(`Can't find ${req.originalUrl} on this server!`, 404)); // Pass an error to the next middleware
+});
+
+app.use(globalErrorHandler); // Global error handling middleware
 
 module.exports = app;
---
+++ 4-natours/utils/appError.js
@@ -0,0 +1,13 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message); // Call the parent class constructor
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true; // Mark this error as operational
+
+        Error.captureStackTrace(this, this.constructor); // Capture the stack trace
+
+    }
+}
+
+module.exports = AppError;
